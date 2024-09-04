ef6dcfbf7c882037498b607765da11fb751df8a5
zwhippo9 <zwhippo9@gmail.com>
2024-09-04 23:06:18 +0000

    Copied Given Program

--- main.cpp
+++ main.cpp
@@ -1,3 +1,24 @@
-#include <iostream>
 
-int main() { std::cout << "Hello World!\n"; }
+//This Program Calculates Percentages based on given variables
+//
+#include <iostream>
+using namespace std;
+int main()
+{
+double salary = 6000;
+double percent1 = 5, percent2 = 7, percent3 = 10;
+double contribution1, contribution2, contribution3;
+ // Calculating Contributions
+contribution1 = (salary * percent1 / 100);
+contribution2 = (salary * percent2 / 100);
+contribution3 = (salary * percent3 / 100);
+ // Displaying Results
+cout << "Amount of contribution at " << percent1 << "% is $" <<
+contribution1 << endl;
+cout << "Amount of contribution at " << percent2 << "% is $" <<
+contribution2 << endl;
+cout << "Amount of contribution at " << percent3 << "% is $" << 
+  contribution3 << endl;
+  
+    return 0;
+}
